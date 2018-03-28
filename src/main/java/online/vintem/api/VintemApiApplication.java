4a58425b264b02194688957b5f0873d74e193668
Eduardo Moreira de Lima <eduardo.lima@basis.com.br>
2018-03-28 18:01:07 -0300

    commit inicial

---
+++ src/main/java/online/vintem/api/VintemApiApplication.java
@@ -0,0 +1,12 @@
+package online.vintem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintemApiApplication.class, args);
+	}
+}
