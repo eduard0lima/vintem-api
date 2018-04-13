829b39f0d65ac16a418a594afaddebe25dbe67bf
Eduardo Moreira de Lima <eduardo.lima@basis.com.br>
2018-04-13 12:49:28 -0300

    add suporte java 8 com api LocalDate

---
+++ src/main/java/online/vintem/api/repository/LancamentoRepository.java
@@ -0,0 +1,11 @@
+package online.vintem.api.repository;
+
+import online.vintem.api.model.Lancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by eduardo on 13/04/18.
+ */
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+}
---
+++ src/main/java/online/vintem/api/resource/LancamentoResource.java
@@ -0,0 +1,35 @@
+package online.vintem.api.resource;
+
+import online.vintem.api.model.Lancamento;
+import online.vintem.api.repository.LancamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by eduardo on 13/04/18.
+ */
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    @GetMapping
+    public List<Lancamento> listar() {
+        return lancamentoRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<?> buscar(@PathVariable Long codigo) {
+        Lancamento lancamento = lancamentoRepository.findOne(codigo);
+        return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+    }
+
+}
