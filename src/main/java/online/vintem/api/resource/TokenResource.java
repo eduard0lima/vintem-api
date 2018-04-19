ae7deaebad4d621bda81ddaee618376d812b75a4
Eduardo Moreira de Lima <eduardo.lima@basis.com.br>
2018-04-19 16:40:36 -0300

    implementado o logout - removido o refresh token do cookie

---
+++ src/main/java/online/vintem/api/resource/TokenResource.java
@@ -0,0 +1,31 @@
+package online.vintem.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by eduardo on 19/04/18.
+ */
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+    @DeleteMapping("revoke")
+    public void revoke(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); // TODO: mudar para true em produção
+        cookie.setPath(request.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+        response.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+
+}
